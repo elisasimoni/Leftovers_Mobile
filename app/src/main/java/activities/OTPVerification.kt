0dd498e0e1e0f3ddb35b65618c39ef21a0c7c197
elisa <elisa.simoni4@studio.unibo.it>
2022-04-19 17:11:37 +0200

    Fix buildinf

---
+++ app/src/main/java/activities/OTPVerification.kt
@@ -0,0 +1,139 @@
+package activities
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Color
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.KeyEvent
+import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
+import android.widget.EditText
+import android.widget.Toast
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.leftovers.R
+
+class OTPVerification : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otp_verification)
+        val otpE1 = findViewById<EditText>(R.id.otpET)
+        val otpE2 = findViewById<EditText>(R.id.otpET2)
+        val otpE3 = findViewById<EditText>(R.id.otpET3)
+        val otpE4 = findViewById<EditText>(R.id.otpET4)
+        val resendButton: TextView = findViewById(R.id.resendOTP)
+        val verifyButton: TextView = findViewById(R.id.verifyBtn)
+        val otpEmail = findViewById<TextView>(R.id.otpEMAIL)
+        val otpMobile = findViewById<TextView>(R.id.mobile_number_text)
+        val getEmail = intent.getStringExtra("EMAIL")
+        val getMobile = intent.getStringExtra("MOBILE")
+        val resendEnable = false
+        val resendTime = 60
+        val selectedETPosition = false
+
+        otpEmail.text = getEmail
+        otpMobile.text = getMobile
+
+        otpE1.addTextChangedListener(textWatcher)
+        otpE2.addTextChangedListener(textWatcher)
+        otpE3.addTextChangedListener(textWatcher)
+        otpE4.addTextChangedListener(textWatcher)
+        //BY DEFAULT OPEN KEYBOARD AT OPE1
+
+        showKeyboard(otpE1)
+        startCountDown(resendEnable, resendButton, resendTime)
+        resendButton.setOnClickListener {
+            if (resendEnable) {
+                startCountDown(resendEnable, resendButton, resendTime)
+            }
+        }
+        verifyButton.setOnClickListener {
+            val generateOTP: String =
+                otpE1.text.toString() + otpE2.text.toString() + otpE3.text.toString() + otpE4.text.toString()
+            if (generateOTP.length == 4) {
+                Toast.makeText(this, "OTP Verified", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "OTP Incorrect", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+    }
+
+    private fun showKeyboard(otpET: EditText) {
+        otpET.requestFocus()
+
+        val inputMenager: InputMethodManager =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMenager.showSoftInput(otpET, 0)
+    }
+
+
+    private fun startCountDown(resendEnable: Boolean, resendButton: TextView, resendTime: Int) {
+
+        resendButton.setTextColor(Color.parseColor("#BDBDBD"))
+
+        object : CountDownTimer((resendTime * 1000).toLong(), 1000) {
+            @SuppressLint("SetTextI18n")
+            override fun onTick(millisUntilFinished: Long) {
+                resendButton.text = "Resend OTP in " + millisUntilFinished / 1000 + "s"
+            }
+
+            @SuppressLint("SetTextI18n")
+            override fun onFinish() {
+                resendButton.text = "Resend OTP"
+                resendButton.setTextColor(Color.parseColor("#FF0000"))
+                resendButton.isEnabled = true
+            }
+        }.start()
+
+
+    }
+
+    private val textWatcher = object : TextWatcher {
+        override fun afterTextChanged(s: Editable) {
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+        }
+    }
+
+    public fun oneKeyUp(
+        E1: EditText,
+        E2: EditText,
+        E3: EditText,
+        E4: EditText,
+        keyCode: Int,
+        event: KeyEvent,
+        setpo: Int
+    ) {
+        var setp = setpo
+        if (keyCode == KeyEvent.KEYCODE_DEL) {
+            if (setp == 3) {
+                setp = 2;
+                showKeyboard(E2)
+            } else if (setp == 2) {
+                E2.setText("")
+                E3.requestFocus()
+            } else if (setp == 3) {
+                E3.setText("")
+                E4.requestFocus()
+            } else if (setp == 4) {
+                E4.setText("")
+                E4.requestFocus()
+            }
+        }
+    }
+}
+
+
+
--- app/src/main/java/activities/RegisterActivity.kt
+++ app/src/main/java/activities/RegisterActivity.kt
@@ -1,14 +1,73 @@
 package activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.InputType
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import com.example.leftovers.R
 
 
 class RegisterActivity : AppCompatActivity() {
+    var passwordShowing = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val email = findViewById<EditText>(R.id.emailET)
+        val mobile = findViewById<EditText>(R.id.mobileET)
+        val password = findViewById<EditText>(R.id.passwordET)
+        val confirmPassword = findViewById<EditText>(R.id.confirm_passwordET)
+        val passwordIcon= findViewById<ImageView>(R.id.password_icon)
+        val confirmPasswordIcon= findViewById<ImageView>(R.id.confirm_password_icon)
+
+        val signUpButton = findViewById<AppCompatButton>(R.id.sign_up_button)
+        val signInButton = findViewById<AppCompatButton>(R.id.sign_in_button)
+
+        passwordIcon.setOnClickListener {
+
+            if(passwordShowing){
+                passwordShowing = false
+                password.inputType=(InputType.TYPE_CLASS_TEXT) ; (InputType.TYPE_TEXT_VARIATION_PASSWORD)
+                passwordIcon.setImageResource(R.drawable.password_show)
+            }
+            else{
+                passwordShowing = true
+                password.inputType=(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD)
+                passwordIcon.setImageResource(R.drawable.password_hide)
+            }
+            password.setSelection(password.text.length)
+        }
+        confirmPasswordIcon.setOnClickListener {
+
+            if(passwordShowing){
+                passwordShowing = false
+                confirmPassword.inputType=(InputType.TYPE_CLASS_TEXT) ; (InputType.TYPE_TEXT_VARIATION_PASSWORD)
+                confirmPasswordIcon.setImageResource(R.drawable.password_show)
+            }
+            else{
+                passwordShowing = true
+                confirmPassword.inputType=(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD)
+                confirmPasswordIcon.setImageResource(R.drawable.password_hide)
+            }
+            confirmPassword.setSelection(confirmPassword.text.length)
+        }
+        signUpButton.setOnClickListener {
+
+
+            Intent(this, OTPVerification::class.java).also {
+                //OTP Verification
+                intent.putExtra("mobile", mobile.text.toString())
+                intent.putExtra("email", email.text.toString())
+                startActivity(it)
+            }
+
+        }
+        signInButton.setOnClickListener {
+            finish()
+        }
     }
 }
 
